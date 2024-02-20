1a89d7ec4d79d3d00e6d431d15e1d1c446915930
Suni777 <121082304+Suni777@users.noreply.github.com>
2024-02-20 06:53:53 -0800

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("<-------Hello World & Wellcome back to jenkins-------->"); 
+        System.out.println("<-------Hello World & Wellcome back to jenkins Webhook-------->"); 
     }
 }
